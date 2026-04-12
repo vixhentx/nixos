a54c8f5848ca8ca7d7d1fd0a8ecaf0aca6ba0f81
vix_hentx <w1084349470@outlook.com>
2026-04-12 19:30:54 +0800

    update(zsh): 历史记录查询功能

--- modules/program/zsh/home.nix
+++ modules/program/zsh/home.nix
@@ -1,11 +1,47 @@
-{ ... }:
+{ config, ... }:
 
 {
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      filter_mode = "global";
+      filter_mode_shell_up_key_binding = "directory";
+      search_mode = "fuzzy";
+      style = "compact";
+      inline_height = 20;
+      workspaces = true;
+      search.filters = [
+        "global"
+        "workspace"
+        "directory"
+        "session"
+        "host"
+      ];
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    history = {
+      path = "${config.xdg.stateHome}/zsh/history";
+      size = 50000;
+      save = 50000;
+      append = true;
+      extended = true;
+      expireDuplicatesFirst = true;
+      findNoDups = true;
+      ignoreAllDups = true;
+      saveNoDups = true;
+    };
+    historySubstringSearch = {
+      enable = true;
+      searchUpKey = [ "^P" ];
+      searchDownKey = [ "^N" ];
+    };
 
     oh-my-zsh = {
       enable = true;
