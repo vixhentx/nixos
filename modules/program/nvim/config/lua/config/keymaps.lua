4592b961da429842471e86fff021940782490c37
vix_hentx <w1084349470@outlook.com>
2026-04-12 12:30:50 +0800

    fix(nvim): 解决叹号等等字符打不出来的问题

--- modules/program/nvim/config/lua/config/keymaps.lua
+++ modules/program/nvim/config/lua/config/keymaps.lua
@@ -2,6 +2,27 @@
 local keymap = vim.keymap.set
 local is_server = vim.g.IsServerMode
 
+local function fix_shifted_number_row()
+    local shifted = {
+        ["<S-1>"] = "!",
+        ["<S-2>"] = "@",
+        ["<S-3>"] = "#",
+        ["<S-4>"] = "$",
+        ["<S-5>"] = "%",
+        ["<S-6>"] = "^",
+        ["<S-7>"] = "&",
+        ["<S-8>"] = "*",
+        ["<S-9>"] = "(",
+        ["<S-0>"] = ")",
+    }
+
+    for lhs, rhs in pairs(shifted) do
+        keymap({ "c", "i" }, lhs, rhs, { noremap = true, silent = true })
+    end
+end
+
+fix_shifted_number_row()
+
 if is_server then
     if vim.g.vscode then
         -- VS Code 专用快捷键
