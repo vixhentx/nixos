4b4cd04f1009060c2aafbd3683e71160a1410242
vix_hentx <w1084349470@outlook.com>
2026-04-12 10:42:18 +0800

    upate(nvim): nvim静默自动更新

--- modules/program/nvim/config/lua/config/lazy.lua
+++ modules/program/nvim/config/lua/config/lazy.lua
@@ -21,15 +21,63 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+local lazy = require("lazy")
+
 -- Setup lazy.nvim
-require("lazy").setup({
+lazy.setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "tokyonight","habamax" } },
+  install = { colorscheme = { "tokyonight", "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = not vim.g.IsServerMode },
+  checker = {
+    enabled = not vim.g.IsServerMode,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
 })
+
+if not vim.g.IsServerMode then
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
+    once = true,
+    callback = function()
+      vim.defer_fn(function()
+        if vim.v.exiting ~= vim.NIL then
+          return
+        end
+
+        local runner = lazy.update({ show = false, wait = false })
+        if not runner then
+          return
+        end
+
+        runner:wait(vim.schedule_wrap(function()
+          local config = require("lazy.core.config")
+          local plugin = require("lazy.core.plugin")
+          local failed = {}
+
+          for _, item in pairs(config.plugins) do
+            if plugin.has_errors(item) then
+              failed[#failed + 1] = item.name
+            end
+          end
+
+          if #failed > 0 then
+            table.sort(failed)
+            vim.notify(
+              "Plugin auto-update failed: " .. table.concat(failed, ", "),
+              vim.log.levels.ERROR,
+              { title = "lazy.nvim" }
+            )
+          end
+        end))
+      end, 1000)
+    end,
+  })
+end
