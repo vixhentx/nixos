c9606d777b443447d544daf539a2f30869276fa0
vix_hentx <w1084349470@outlook.com>
2026-05-24 21:31:43 +0800

    fix(nvim): 修复傻逼硬编码导致的nvim配置无效, 干掉之前丑的一b的写法.

--- modules/program/nvim/home.nix
+++ modules/program/nvim/home.nix
@@ -1,8 +1,6 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 let
   inherit (config.lib.file) mkOutOfStoreSymlink;
-  nvimConfig = "${config.home.homeDirectory}/.config/nixos/modules/program/nvim/config";
-  nvimConfigFile = path: mkOutOfStoreSymlink "${nvimConfig}/${path}";
 in
 {
   home.packages = with pkgs; [
@@ -11,35 +9,15 @@ in
     git
     gnumake
     nixd
-    nixfmt
+    nixfmt-rfc-style
     ripgrep
     tree-sitter
   ];
 
   xdg.configFile = {
-    "nvim/init.lua".source = nvimConfigFile "init.lua";
-    "nvim/lua/config/keymaps.lua".source = nvimConfigFile "lua/config/keymaps.lua";
-    "nvim/lua/config/lazy.lua".source = nvimConfigFile "lua/config/lazy.lua";
-    "nvim/lua/config/lsp.lua".source = nvimConfigFile "lua/config/lsp.lua";
-    "nvim/lua/config/server_opt.lua".source = nvimConfigFile "lua/config/server_opt.lua";
-    "nvim/lua/plugins/core.lua".source = nvimConfigFile "lua/plugins/core.lua";
-    "nvim/lua/plugins/init.lua".source = nvimConfigFile "lua/plugins/init.lua";
-    "nvim/lua/plugins/tools.lua".source = nvimConfigFile "lua/plugins/tools.lua";
-    "nvim/lua/plugins/ui.lua".source = nvimConfigFile "lua/plugins/ui.lua";
-
-    # nvimpager defaults to ~/.config/nvimpager instead of ~/.config/nvim.
-    # Point it back to the managed nvim config so both share the same setup.
-    "nvimpager".source = mkOutOfStoreSymlink "${config.xdg.configHome}/nvim";
+    "nvim".source = ./config;
+    "nvimpager".source = ./config;
   };
 
-  # Share lazy.nvim data/plugins with nvimpager as well, otherwise it will
-  # look in ~/.local/share/nvimpager and miss plugins installed under nvim.
   xdg.dataFile."nvimpager".source = mkOutOfStoreSymlink "${config.xdg.dataHome}/nvim";
-
-  home.activation.seedNvimLazyLock = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    lockfile="${config.xdg.configHome}/nvim/lazy-lock.json"
-    if [ ! -e "$lockfile" ]; then
-      $DRY_RUN_CMD ${pkgs.coreutils}/bin/install -Dm0644 ${nvimConfig}/lazy-lock.json "$lockfile"
-    fi
-  '';
 }
